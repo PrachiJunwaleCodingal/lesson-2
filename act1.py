e41dfe9c84507fd2c0f562848e0eb088b9cd6fe1
PrachiJunwaleCodingal <prachi.junwale@gmail.com>
2024-12-20 17:25:31 +0530

    hi

---
+++ act1.py
@@ -0,0 +1,19 @@
+print("Hi, This is Raj.\n")
+print("Birthdays of my 5 friends are\n")
+
+priyaVar="12th Feb"
+print("Priya's birthday is on  -",priyaVar)
+
+pihuVar="18th June"
+print("Pihu's birthday is on   -",pihuVar)
+
+AryaveerVar="20th Feb"
+print("Priya's birthday is on  -",AryaveerVar)
+
+RajpriyaVar="19th Dec"
+print("Priya's birthday is on  -",RajpriyaVar)
+
+AdishVar="30th Nov"
+print("Adish's birthday is on  -",AdishVar)
+
+
